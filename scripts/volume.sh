7d18b3ba80eb0571fc36c1ca57dab13c56028694
hafermoraes <hafer.moraes@gmail.com>
2022-06-28 17:31:47 -0300

    fix: volume.sh com script alternativo

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -4,6 +4,7 @@ vol=""
 
 # amixer
 #vol+=$(awk -F'[][]' '/dB/ {print $2 }' <(amixer sget Master))
+#vol+=$(awk -F'[][]' '/dB/ {print $2 }' <(amixer -c 0 -M -D pulse get Master))
 
 # pactl
 vol+=$(pactl list sinks | grep -i volume: | awk '{print $5}')
